c8e9e44e4625c46cfbf3294af0a56b93f8330cbc
Esau Jesse <jesseesau13@gmail.com>
2025-05-19 10:30:26 +0100

    Loop through an array

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let count = 0;
+let len = array.length;
+
+// Loop through array
+while (count < len) {
+    console.log(array[count]);
+    count++;
+}
