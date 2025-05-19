b5b22e0f5ac5533d2b680991b4d42ef983a4cdea
Esau Jesse <jesseesau13@gmail.com>
2025-05-19 09:43:39 +0100

    Remove length. use simple if else

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,9 @@
 const { argv } = require('node:process');
 
 // The two commands used to run the script don't count
-if (argv.length == 2) {
-    console.log("No argument")
+if (argv[2]) {
+    console.log(argv[2]);
 }
 else {
-    console.log(argv[2])
+    console.log("No argument");
 }
